1f00afe60625c491be587f0605fb2f5eb244302e
MuhammadDaffaAzakki <daffaazakki29@gmail.com>
2025-03-18 16:51:30 +0700

    Membuat Program Switch Pilihan Case 1

--- file3.cpp
+++ file3.cpp
@@ -19,3 +19,16 @@ int main(){
         cout << "2. Menghitung Luas Lingkaran" << endl;
         cout << "3. Exit" << endl;
         cin >> pilihan;
+
+        switch(pilihan) {
+            case 1 :
+                cout << "Menghitung Luas Persegi Panjang" << endl;
+                cout << "Masukkan Panjang : ";
+                cin >> panjang;
+                cout << " Masukkan Lebar : ";
+                cin >> lebar;
+                cout << "Luas Persegi Panjang : " <<
+                LuasPersegiPanjang(panjang, lebar) << endl;
+                cout << endl;
+                break;
+        
